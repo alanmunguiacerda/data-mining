9f830cfc6abc06d911944c388d41906eb3decaa1
Alan Munguia <alanmunguiacerda@hotmail.com>
2016-11-20 22:38:47 -0600

    Numeric correlation

--- src/analisis/Correlation.py
+++ src/analisis/Correlation.py
@@ -2,16 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division
+import numpy
 from collections import Counter
 from JarvisMath import JarvisMath
+from Usseful import index_in_range
+
 
 class Correlation():
-    jm = JarvisMath()
-
     def __init__(self):
         pass
 
-    def nominal(self, data, attribute_1, attribute_2):
+    @staticmethod
+    def nominal(data, attribute_1, attribute_2):
+        if not index_in_range(attribute_1, data) or not index_in_range(attribute_2, data):
+            return None
+
         frequency_table = Counter([(x[attribute_1], x[attribute_2]) for x in data])
         counter_attribute_1 = Counter(x[attribute_1] for x in data)
         counter_attribute_2 = Counter(x[attribute_2] for x in data)
@@ -22,6 +27,21 @@ class Correlation():
                 x = frequency_table[(key, key2)]
                 calc = pow(x - e, 2) / e
                 x2.append(calc)
-
+        jm = JarvisMath()
         free_levels = (len(counter_attribute_1) - 1) * (len(counter_attribute_2) - 1)
-        return self.jm.chi_square(sum(x2), free_levels)
+        return jm.chi_square(sum(x2), free_levels)
+
+    @staticmethod
+    def numeric(data, attribute_1, attribute_2):
+        if not index_in_range(attribute_1, data) or not index_in_range(attribute_2, data):
+            return None
+
+        list_attribute_1 = [x[attribute_1] for x in data]
+        list_attribute_2 = [x[attribute_2] for x in data]
+        mean_1 = numpy.mean(list_attribute_1)
+        mean_2 = numpy.mean(list_attribute_2)
+        desvest_1 = numpy.std(list_attribute_1)
+        desvest_2 = numpy.std(list_attribute_2)
+        n = len(data)
+        partial = sum([(x[attribute_1] - mean_1)*(x[attribute_2]- mean_2) for x in data])
+        return partial / (n * desvest_1 * desvest_2)
