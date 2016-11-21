244002648d79e2be7359ccb8ec1974e916b8f8b9
Alan Munguia <alanmunguiacerda@hotmail.com>
2016-11-21 11:59:30 -0600

    Fixed chi_square json path

--- src/constants.py
+++ src/constants.py
@@ -9,4 +9,4 @@ ADD_MARKUP_STRING = '<span foreground="green">Add</span>'
 DEL_MARKUP_STRING = '<span foreground="red">Del</span>'
 MOD_MARKUP_STRING = '<span foreground="blue">Mod</span>'
 SPAN_MARKUP_REGEXP = '(</?span([a-z]|=|\"|\s)*>)'
-CHI_VALUES_DIR = os.path.abspath('abs_values/chi_square.json')
+CHI_VALUES_DIR = os.path.abspath('analisis/abs_values/chi_square.json')
